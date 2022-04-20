2720ee68174e6ca88892f90d7f07fd8b37e617fc
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 06:43:30 +0530

    inital commit

---
+++ src/main/kotlin/io/cherrytechnologies/photoappeurekaservice/PhotoAppEurekaServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.cherrytechnologies.photoappeurekaservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotoAppEurekaServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PhotoAppEurekaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/io/cherrytechnologies/photoappeurekaservice/PhotoAppEurekaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.cherrytechnologies.photoappeurekaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoAppEurekaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
