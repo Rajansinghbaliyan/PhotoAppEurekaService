b6930b4f4c5257d256677290fbf96b70b8d0da88
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 07:11:34 +0530

    fix the configuration

--- src/main/kotlin/io/cherrytechnologies/photoappeurekaservice/PhotoAppEurekaServiceApplication.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappeurekaservice/PhotoAppEurekaServiceApplication.kt
@@ -2,8 +2,10 @@ package io.cherrytechnologies.photoappeurekaservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class PhotoAppEurekaServiceApplication
 
 fun main(args: Array<String>) {
